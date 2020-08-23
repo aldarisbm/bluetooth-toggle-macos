f4d66690b4433b96dfcdc2ba54c2f2b96b0e23e7
Jose Berrio <aldarisbm@gmail.com>
2020-08-22 19:55:31 -0500

    working on it

--- togglebt.sh
+++ togglebt.sh
@@ -3,6 +3,9 @@ LID_OPEN=$(ioreg -r -k AppleClamshellState | grep AppleClamshellState | cut -d \
 CONNECTED_TO_POWER=$(pmset -g batt | head -n 1 | cut -d \' -f2)
 
 if [[ "${LID_OPEN}" -eq "No" && "${CONNECTED_TO_POWER}" -eq "Battery Power" ]]
+then
     blueutil -p 0
+    echo "Lid is closed & bluetooth should be off"
 else
-    blue
+    echo "Lid is open & bluetooth should be resumed"
+    blueutil -p 1
