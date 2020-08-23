9d5dadca1e69fa9a26deb673d7b37461d51f46fc
Jose Berrio <aldarisbm@gmail.com>
2020-08-22 23:41:24 -0500

    corrected string equals

--- togglebt.sh
+++ togglebt.sh
@@ -2,10 +2,11 @@ LID_OPEN=$(ioreg -r -k AppleClamshellState | grep AppleClamshellState | cut -d \
 
 CONNECTED_TO_POWER=$(pmset -g batt | head -n 1 | cut -d \' -f2)
 
-if [[ "${LID_OPEN}" -eq "No" && "${CONNECTED_TO_POWER}" -eq "Battery Power" ]]
+if [[ "${LID_OPEN}" == "No" && "${CONNECTED_TO_POWER}" == "Battery Power" ]]
 then
     blueutil -p 0
-    echo "Lid is closed & bluetooth should be off"
+    echo "Lid is closed & bluetooth should be off" >> lidclosed.txt
 else
-    echo "Lid is open & bluetooth should be resumed"
+    echo "Lid is open & bluetooth should be resumed" >> lidopen.txt
     blueutil -p 1
+fi
